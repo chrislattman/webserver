fcdcc3f4a59a0fada4b05e55dd3b69c766b26c3c
Chris Lattman <natgeo13@gmail.com>
2022-04-09 17:38:50 -0400

    update python server

--- server.py
+++ server.py
@@ -2,29 +2,30 @@ import socket
 import time
 from threading import Thread
 
-
 DEFAULT_PORT = 80
 INT_MAX = 2147483647
 
+
 class ClientHandler(Thread):
-    def __init__(self, client_socket, client_address):
+    """Thread for client"""
+
+    def __init__(self, client_socket_arg, client_address_arg):
         Thread.__init__(self)
-        self.client_socket = client_socket
-        self.client_address = client_address
+        self.client_socket = client_socket_arg
+        self.client_address = client_address_arg
 
     def run(self):
-        server_message = 'HTTP/1.1 200 OK\n'
-        server_message += time.strftime('Date: %a, %d %b %Y %X GMT\n', 
-            time.gmtime())
-        server_message += 'Server: Web Server\n'
-        server_message += 'Last-Modified: Fri, 08 Apr 2022 12:35:05 GMT\n'
-        server_message += 'Accept-Ranges: bytes\n'
+        server_message = "HTTP/1.1 200 OK\n"
+        format_string = "Date: %a, %d %b %Y %X GMT\n"
+        server_message += time.strftime(format_string, time.gmtime())
+        server_message += "Server: Web Server\n"
+        server_message += "Last-Modified: Fri, 08 Apr 2022 12:35:05 GMT\n"
+        server_message += "Accept-Ranges: bytes\n"
 
-        content = 'What\'s up? Your IP address is {}\n'.format(
-            self.client_address)
+        content = f"What's up? Your IP address is {self.client_address}\n"
 
-        server_message += 'Content-Length: {}\n'.format(len(content))
-        server_message += 'Content-Type: text/html\n\n'
+        server_message += f"Content-Length: {len(content)}\n"
+        server_message += "Content-Type: text/html\n\n"
         server_message += content
 
         self.client_socket.send(server_message.encode())
@@ -35,7 +36,7 @@ class ClientHandler(Thread):
 threads = []
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-server_address = ('', 80)
+server_address = ("", 80)
 
 server_socket.bind(server_address)
 server_socket.listen(INT_MAX)
